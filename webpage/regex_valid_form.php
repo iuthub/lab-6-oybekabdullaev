1525fcf8db1c058f5ca5c5b4751c0eb9fa1efc83
Oybek Abdullaev <o.abdullaev2@student.inha.uz>
2020-03-18 10:15:26 +0500

    complete lab 7

--- webpage/regex_valid_form.php
+++ webpage/regex_valid_form.php
@@ -1,18 +1,14 @@
-<?php
+<?php 
 
 	$pattern="";
 	$text="";
 	$replaceText="";
-	$replacedText="";
 
 	$match="Not checked yet.";
 
 if ($_SERVER["REQUEST_METHOD"]=="POST") {
 	$pattern=$_POST["pattern"];
 	$text=$_POST["text"];
-	$replaceText=$_POST["replaceText"];
-
-	$replacedText=preg_replace($pattern, $replaceText, $text);
 
 	if(preg_match($pattern, $text)) {
 						$match="Match!";
@@ -31,27 +27,24 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
 	<title>Valid Form</title>
 </head>
 <body>
-	<form action="regex_valid_form.php" method="post">
+	<form action="valid_form.php" method="post">
 		<dl>
 			<dt>Pattern</dt>
 			<dd><input type="text" name="pattern" value="<?= $pattern ?>"></dd>
-
+			
 			<dt>Text</dt>
 			<dd><input type="text" name="text" value="<?= $text ?>"></dd>
-
+			
 			<dt>Replace Text</dt>
 			<dd><input type="text" name="replaceText" value="<?= $replaceText ?>"></dd>
 
 			<dt>Output Text</dt>
 			<dd><?=	$match ?></dd>
 
-			<dt>Replaced Text</dt>
-			<dd> <code><?=	$replacedText ?></code></dd>
-
 			<dt>&nbsp;</dt>
 			<dd><input type="submit" value="Check"></dd>
 		</dl>
-
+		
 	</form>
 </body>
 </html>
